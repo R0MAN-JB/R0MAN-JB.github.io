0842d89513254e6c41ee8cd08f497f0be402efc8
R0MAN-JB <94933116+R0MAN-JB@users.noreply.github.com>
2021-11-23 17:12:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('Sabia que tomarias la mejor decicion tkm :3');
+    alert('Sabia que tomarias la mejor decicion tqm :3');
 });
 
 const noBtn = document.querySelector('#noBtn');
